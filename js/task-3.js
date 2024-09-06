58cee5f612517c95dfade667b70f60e0aacf987f
Pasha Nikolauchyk <pasha.nikolauchyk044@ukr.net>
2024-09-06 13:43:20 +0300

    mod3.1

--- js/task-3.js
+++ js/task-3.js
@@ -13,20 +13,3 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-const emptyArray = [];
-const nonEmptyArray = [1, 2, 3];
-
-console.log(Boolean(emptyArray)); // true
-console.log(Boolean(nonEmptyArray)); // true
-
-if (emptyArray) {
-  console.log("if is in progress");
-} else {
-  console.log("else is not performed");
-}
-
-if (nonEmptyArray) {
-  console.log("if is in progress");
-} else {
-  console.log("else is not performed");
-}
